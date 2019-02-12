932a79bd618f3dbc0c23a1b9d0085d29509b3dd1
ppnman <zjli1997@qq.com>
2019-02-11 22:54:04 -0800

    Correct the name of liblua-dev

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ install_vim8_from_source()
     sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev
+    python3-dev ruby-dev lua5.1 liblua5.1-0-dev libperl-dev
     
     # step2: download source code.
     git clone https://github.com/vim/vim.git
