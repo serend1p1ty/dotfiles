f0b8fa70e7565c1d65ee1124e8f4778705560651
520Chris <zjli1997@163.com>
2019-12-30 16:37:04 +0800

    Update tmux statusline

--- tools/update.sh
+++ tools/update.sh
@@ -1,12 +1,22 @@
 path=$(cd "$(dirname "$0")";pwd)
 
 if [ $1 == "up" ]; then
-    echo "Upload vim configuration."
+    echo "Upload configuration."
+
     cp ~/.config/nvim/* $path/../vim -rf
     rm $path/../vim/plugged -rf
+
+    cp ~/.tmux.conf $path/../tmux.conf
+    cp ~/.zshrc $path/../zshrc
+    cp ~/.gitconfig $path/../gitconfig
+    cp ~/vscode.vimrc $path/../vscode.vimrc
 fi
 
 if [ $1 == "down" ]; then
-    echo "Download vim configuration."
+    echo "Download configuration."
     cp $path/../vim/* ~/.config/nvim -rf
+    cp $path/../tmux.conf ~/.tmux.conf
+    cp $path/../zshrc ~/.zshrc
+    cp $path/../gitconfig ~/.gitconfig
+    cp $path/../vscode.vimrc ~/vscode.vimrc
 fi
